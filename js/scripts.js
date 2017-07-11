46dc3dd5ba96f17a865a6a2c71c3f1f9e185e7da
Bart Webber and Trevor Gill <trevor@>
2017-07-11 11:37:27 -0700

    add function in JS file to replace a with hypen.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,47 @@
 var vowels = ["a", "e", "i", "o", "u"];
+var sentenceArray = [];
+var currentVowel = "";
+var currentLetter = "";
 
 $(document).ready(function(){
   $("#wordForm").submit(function(event){
     var sentenceInput = $("input#inputWords").val();
-    // var sentenceObscured = sentenceInput.replaceAll = function(/a/i, "-") {
-    //   var target = this;
-    //   alert(target.split(/a/gi).join("-");
-    // };
-    // alert(sentenceObscured);
-    var sentenceObscured = sentenceInput.replace(/a/gi, "-");
-    var sentenceObscured = sentenceObscured.replace(/e/gi, "-");
-    var sentenceObscured = sentenceObscured.replace(/i/gi, "-");
-    var sentenceObscured = sentenceObscured.replace(/o/gi, "-");
-    var sentenceObscured = sentenceObscured.replace(/u/gi, "-");
-    alert(sentenceObscured);
+    var letterArray = sentenceInput.split("");
 
-    
+    for(var index = 0; index < letterArray.length; index += 1){
+      currentLetter = letterArray[index];
+      if(currentLetter === "a") {
+        currentLetter = "-";
+        
+        alert(currentLetter);
+      } else if(currentLetter === "e") {
+        currentLetter = "-";
+      } else if(currentLetter === "i") {
+        currentLetter = "-";
+      } else if(currentLetter === "o") {
+        currentLetter = "-";
+      } else if(currentLetter === "u") {
+        currentLetter = "-";
+      } else {
+        currentLetter = currentLetter
+      }
+    }
 
 
-
-
+    // REGEX SOLUTION
+    // var sentenceObscured = sentenceInput.replace(/a/gi, "-");
+    // var sentenceObscured = sentenceObscured.replace(/e/gi, "-");
+    // var sentenceObscured = sentenceObscured.replace(/i/gi, "-");
+    // var sentenceObscured = sentenceObscured.replace(/o/gi, "-");
+    // var sentenceObscured = sentenceObscured.replace(/u/gi, "-");
+    // alert(sentenceObscured)
 
     // var target = this;
     // alert(target.split("a").join("-");
 
-
-
     event.preventDefault();
-  })
-})
+  });
+});
 
 
 
